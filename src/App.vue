54d3cfa74fa009409a872d622f7a75748983594f
xiesongjian <xiesongjian@dataeye.com>
2024-10-22 01:26:04 +0800

    feat: 重构了任务网格图的实现方式，优化了图表的响应式和动态效果，提高了用户体验。

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,19 @@
       backgroundColor: #000;
       margin: 0 auto;
       opcity: 0.5;
-    " v-resize="resize"></div>
+    "></div>
 </template>
 
 <script>
 export default {
   name: "rwwgEcharts",
   data() {
-    return {};
+    return {
+      chart: null,
+      data: [],
+      timer: null,
+      seriesData: []
+    };
   },
   /* 自定义缩放指令 */
   directives: {
@@ -38,47 +43,15 @@ export default {
   },
   computed: {},
   mounted() {
-    this.getRwwg();
+    this.initChart();
   },
   methods: {
-    getRwwg() {
-      let lb1 = ["待执行", "执行成功"];
-      let lb2 = ["待执行", "待下发", "下达成功", "执行成功"];
-      let lb3 = ["待下发", "下达成功", "执行失败"];
-      let lb4 = ["待执行", "执行成功"];
-      let lb5 = ["待下发", "下达成功", "执行成功"];
+    async initChart() {
+      this.chart = this.$echarts.init(document.getElementById("echarts_main"));
 
-      let rwwgCharts = this.$echarts.init(
-        document.getElementById("echarts_main")
-      );
-
-      let symbolSize = 20;
-      let data = [
-        { x: 3, y: "待执行" },
-        { x: 22, y: "执行成功" },
-      ];
-      let data2 = [
-        { y: "待执行", x: 3 },
-        { y: "待下发", x: 13 },
-        { y: "下达成功", x: 26 },
-        { y: "执行成功", x: 33 },
-      ];
-      let data3 = [
-        { y: "待下发", x: 5 },
-        { y: "下达成功", x: 20 },
-        { y: "执行失败", x: 30 },
-      ];
-      let data4 = [
-        { y: "待执行", x: 3 },
-        { y: "执行成功", x: 40 },
-      ];
-      let data5 = [
-        { y: "待下发", x: 9 },
-        { y: "下达成功", x: 28 },
-        { y: "执行成功", x: 42 },
-      ];
-      const allData = [data, data2, data3, data4, data5];
-
+      this.data = await this.getData()
+      this.seriesData = this.getSeriesData(0);
+      const xAxisData = this.data.list.map(m => m[0].x)
       let option = {
         title: {
           text: "任务网格图",
@@ -103,21 +76,24 @@ export default {
         },
         grid: {},
         xAxis: {
-          min: 0,
-          max: 50,
-          type: "value",
+          // min: 0,
+          // max: 50,
+          // type: "value",
           axisLine: { onZero: false },
           color: "#00ffff",
+          type: "category",
+          data: xAxisData,
         },
         axisLabel: {
           // formatter: '{value}位',
           show: true,
           color: "#00ffff",
-          formatter: (val) => {
-            console.log(val);
-            return "T" + `${-val}`;
-          },
+          // formatter: (val) => {
+          //   console.log(val);
+          //   return "T" + `${-val}`;
+          // },
         },
+
         yAxis: {
           type: "category",
           data: ["执行失败", "执行成功", "下达成功", "待下发", "待执行"],
@@ -156,122 +132,105 @@ export default {
             filterMode: "empty",
           },
         ],
-        series: [
-          {
-            name: "原任务1",
-            id: "a",
-            type: "line",
-            smooth: true,
-            symbolSize: symbolSize,
-            data: data.map(function (item) {
-              return [item.x, item.y];
-            }),
-            itemStyle: {
-              borderRadius: [0, 10, 10, -3],
-              color: "#00ffff",
-              // borderWidth: 1,
-              // borderColor: "skyblue"
-            },
-            label: {
-              show: true,
-              formatter: function (params) {
-                return lb1[params.dataIndex];
-              },
-            },
-          },
-          {
-            name: "原任务2",
-            id: "b",
-            type: "line",
-            smooth: true,
-            symbolSize: symbolSize,
-            data: data2.map(function (item) {
-              return [item.x, item.y];
-            }),
-            itemStyle: {
-              borderRadius: [0, 10, 10, -3],
-              color: "#FFFE27",
-              // borderWidth: 1,
-              // borderColor: "skyblue"
-            },
-            label: {
-              show: true,
-              formatter: function (params) {
-                return lb2[params.dataIndex];
-              },
-            },
-          },
-          {
-            name: "原任务3",
-            id: "c",
-            type: "line",
-            smooth: true,
-            symbolSize: symbolSize,
-            data: data3.map(function (item) {
-              return [item.x, item.y];
-            }),
-            itemStyle: {
-              borderRadius: [0, 10, 10, -3],
-              color: "#F58787",
-            },
-            label: {
-              show: true,
-              formatter: function (params) {
-                return lb3[params.dataIndex];
-              },
-            },
-          },
-          {
-            name: "原任务4",
-            id: "d",
-            type: "line",
-            smooth: true,
-            symbolSize: symbolSize,
-            data: data4.map(function (item) {
-              return [item.x, item.y];
-            }),
-            itemStyle: {
-              borderRadius: [0, 10, 10, -3],
-              color: "#CDF296",
-            },
-            label: {
-              show: true,
-              formatter: function (params) {
-                return lb4[params.dataIndex];
-              },
-            },
-          },
-          {
-            name: "原任务5",
-            id: "e",
-            type: "line",
-            smooth: true,
-            symbolSize: symbolSize,
-            data: data5.map(function (item) {
-              return [item.x, item.y];
-            }),
-            itemStyle: {
-              borderRadius: [0, 10, 10, -3],
-              color: "#D1AEEC",
-            },
-            label: {
-              show: true,
-              formatter: function (params) {
-                return lb5[params.dataIndex];
-              },
-            },
-          },
-        ],
+        series: this.seriesData
       };
 
-      this.rwwg = option;
+      this.chart.setOption(option);
 
-      rwwgCharts.setOption(option);
+      this.runTimer()
 
       // 根据页面大小自动响应图表大小
-      //   window.addEventListener("resize", () => {
-      //     rwwgCharts.resize();
-      //   });
+      window.addEventListener("resize", () => {
+        this.chart.resize();
+      });
+    },
+
+    runTimer() {
+      const len = this.data.length
+      let i = 0
+      this.timer = setInterval(() => {
+        i++
+        const seriesData = this.getSeriesData(i)
+        this.chart.setOption({
+          series: seriesData
+        });
+        if (i === len) {
+          clearInterval(this.timer)
+        }
+      }, 2000);
+    },
+    getSeriesData(idx) {
+      let symbolSize = 20;
+
+      function createSeries(list) {
+        const colors = ['#00ffff', '#FFFE27', '#F58787', '#CDF296', '#D1AEEC', '#B2D6FF', '#FFB2B2', '#FFD1AEEC', '#FFB2D6FF'];
+        return list.map((item, index) => {
+          return {
+            name: "原任务" + (index + 1),
+            id: index,
+            type: "line",
+            smooth: true,
+            symbolSize: symbolSize,
+            data: item.map(function (it) {
+              return [index <= idx ? it.x : '', it.y];
+            }),
+            itemStyle: {
+              borderRadius: [0, 10, 10, -3],
+              color: colors[index],
+              // borderWidth: 1,
+              // borderColor: "skyblue"
+            },
+            label: {
+              show: true,
+              formatter: function (params) {
+                return params.value[1]
+              },
+            },
+          }
+        })
+      }
+      const result = createSeries(this.data.list, idx)
+      return result
+    },
+    getData() {
+      return new Promise((resolve) => {
+        let data = [
+          { x: '原任务1', y: "待执行" },
+          { x: '原任务1', y: "执行成功" },
+        ];
+        let data2 = [
+          { y: "待执行", x: '原任务2' },
+          { y: "待下发", x: '原任务2' },
+          { y: "下达成功", x: '原任务2' },
+          { y: "执行成功", x: '原任务2' },
+        ];
+        let data3 = [
+          { y: "待下发", x: '原任务3' },
+          { y: "下达成功", x: '原任务3' },
+          { y: "执行失败", x: '原任务3' },
+        ];
+        let data4 = [
+          { y: "待执行", x: '原任务4' },
+          { y: "执行成功", x: '原任务4' },
+        ];
+        let data5 = [
+          { y: "待下发", x: '原任务5' },
+          { y: "下达成功", x: '原任务5' },
+          { y: "执行成功", x: '原任务5' },
+        ];
+        const res = {
+          list: [
+            data,
+            data2,
+            data3,
+            data4,
+            data5
+          ]
+        }
+        setTimeout(() => {
+          resolve(res)
+        }, 100);
+      })
     },
     resize() {
       let charts = this.$echarts.init(document.getElementById("echarts_main"));
